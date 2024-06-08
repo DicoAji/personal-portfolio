19032b4c884b3d9a1c3e519271a40192f7f341ee
DicoAji <dicoaji12@gmail.com>
2024-06-09 00:29:46 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,25 @@ function App() {
       duration: 3000,
       origin: "left",
       distance: "300px",
-      reset: true,
     });
     ScrollReveal().reveal(".slide-bottom", {
       duration: 3000,
       origin: "top",
       distance: "300px",
-      reset: true,
     });
     ScrollReveal().reveal(".slide-left", {
       duration: 3000,
       origin: "right",
       distance: "300px",
-      reset: true,
     });
     ScrollReveal().reveal(".slide-top", {
       duration: 3000,
       origin: "bottom",
       distance: "300px",
-      reset: true,
     });
     ScrollReveal().reveal(".rotate", {
       duration: 3000,
-      rotate: { x: 0, y: 0, z: 360 }, // Rotasi 360 derajat di sumbu Z
-      reset: true, // Menjalankan animasi kembali saat elemen terlihat
+      rotate: { x: 0, y: 0, z: 360 }, // Menjalankan animasi kembali saat elemen terlihat
     });
     const textPathElement = document.querySelector(".text-curve");
     textPathElement.classList.add("rotate-animation");
